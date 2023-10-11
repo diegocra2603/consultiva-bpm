085726abcf7f73400568d9c122459dafd41cf377
Diego José Cruz <84749586+diegocra2603@users.noreply.github.com>
2023-10-11 18:23:24 +0000

    Se agrego cambios a la forma en que se muestran los iconos en la parte de shurtcuts

--- src/app/layout/common/shortcuts/components/add-edit-mode/add-edit-mode.component.ts
+++ src/app/layout/common/shortcuts/components/add-edit-mode/add-edit-mode.component.ts
@@ -1,6 +1,21 @@
-import { AsyncPipe, NgForOf, NgIf } from '@angular/common';
-import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit, Output, ViewChild, ViewEncapsulation } from '@angular/core';
-import { FormControl, FormsModule, ReactiveFormsModule, UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
+import { AsyncPipe, TitleCasePipe, NgForOf, NgIf } from '@angular/common';
+import {
+    ChangeDetectionStrategy,
+    Component,
+    EventEmitter,
+    Input,
+    Output,
+    ViewChild,
+    ViewEncapsulation,
+} from '@angular/core';
+import {
+    FormControl,
+    FormsModule,
+    ReactiveFormsModule,
+    UntypedFormBuilder,
+    UntypedFormGroup,
+    Validators,
+} from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
@@ -15,94 +30,110 @@ import { MatIconModule } from '@angular/material/icon';
 import { IconsService } from '../../icon.service';
 
 @Component({
-  selector: 'add-edit-mode',
-  templateUrl: './add-edit-mode.component.html',
-  encapsulation: ViewEncapsulation.None,
-  changeDetection: ChangeDetectionStrategy.OnPush,
-  exportAs: 'add-edit-mode',
-  standalone: true,
-  imports: [FormsModule, ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatSlideToggleModule,  MatIconModule, MatButtonModule, NgIf, MatSelectModule, NgForOf, MatOptionModule, NgxMatSelectSearchModule, AsyncPipe]
+    selector: 'add-edit-mode',
+    templateUrl: './add-edit-mode.component.html',
+    encapsulation: ViewEncapsulation.None,
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    exportAs: 'add-edit-mode',
+    standalone: true,
+    imports: [
+        FormsModule,
+        ReactiveFormsModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatSlideToggleModule,
+        MatIconModule,
+        MatButtonModule,
+        NgIf,
+        MatSelectModule,
+        NgForOf,
+        MatOptionModule,
+        NgxMatSelectSearchModule,
+        AsyncPipe,
+        TitleCasePipe,
+    ],
 })
 export class AddEditModeComponent {
+    @Input() mode: ModeShortCut;
 
-  @Input() mode: ModeShortCut;
-
-  @Input() set shortcut(value: Shortcut) {
-    this.shortcutForm.patchValue(value);
-  }
-
-  @Output() save: EventEmitter<Shortcut> = new EventEmitter<Shortcut>();
-
-  @Output() delete: EventEmitter<Shortcut> = new EventEmitter<Shortcut>();
-
-  shortcutForm: UntypedFormGroup = this._formBuilder.group({
-    id: [''],
-    label: ['', [Validators.required]],
-    description: ['', [Validators.required]],
-    icon: ['Hola mundo', [Validators.required]],
-    link: ['', [Validators.required]],
-    useRouter: [false, [Validators.nullValidator]],
-  });
-
-  protected icons: string[] = [];
-
-  public iconFilterCtrl: FormControl<string> = new FormControl<string>('');
-
-  public filteredIcons: ReplaySubject<string[]> = new ReplaySubject<string[]>(1);
-
-  @ViewChild('singleSelect', { static: true }) singleSelect: MatSelect;
-
-  protected _onDestroy = new Subject<void>();
-
-  constructor(
-    private _formBuilder: UntypedFormBuilder,
-    private _iconsService: IconsService) { }
-
-  ngOnInit(): void {
-
-    this._iconsService.icons
-      .pipe(takeUntil(this._onDestroy))
-      .subscribe((icons) => {
-        const namespace = icons.namespace;
-        const iconsList = icons.list;
-        this.icons = iconsList.map( x => `${namespace}:${x}`)
-      });
-
-    this.filteredIcons.next(this.icons.slice());
-
-    this.iconFilterCtrl.valueChanges
-      .pipe(takeUntil(this._onDestroy))
-      .subscribe(() => {
-        this.filterIcons();
-      });
-  }
-
-  setIconValue(value: string[]) {
-    this.shortcutForm.controls['icon'].setValue(value.join(' '));
-  }
-
-  protected filterIcons() {
-
-    if (!this.icons) {
-      return;
+    @Input() set shortcut(value: Shortcut) {
+        this.shortcutForm.patchValue(value);
     }
 
-    let search = this.iconFilterCtrl.value;
-    if (!search) {
-      this.filteredIcons.next(this.icons.slice());
-      return;
-    } else {
-      search = search.toLowerCase();
-    }
+    @Output() save: EventEmitter<Shortcut> = new EventEmitter<Shortcut>();
 
-    this.filteredIcons.next(
-      this.icons.filter(string => string.toLowerCase().indexOf(search) > -1)
+    @Output() delete: EventEmitter<Shortcut> = new EventEmitter<Shortcut>();
+
+    shortcutForm: UntypedFormGroup = this._formBuilder.group({
+        id: [''],
+        label: ['', [Validators.required]],
+        description: ['', [Validators.required]],
+        icon: ['', [Validators.required]],
+        link: ['', [Validators.required]],
+        useRouter: [false, [Validators.nullValidator]],
+    });
+
+    protected icons: any[] = [];
+
+    public iconFilterCtrl: FormControl<string> = new FormControl<string>('');
+
+    public filteredIcons: ReplaySubject<string[]> = new ReplaySubject<string[]>(
+        1,
     );
 
-  }
+    @ViewChild('singleSelect', { static: true }) singleSelect: MatSelect;
 
-  internalSave() {
-    this.save.emit(this.shortcutForm.value)
-    this.shortcutForm.reset();
-  }
+    protected _onDestroy = new Subject<void>();
+
+    constructor(
+        private _formBuilder: UntypedFormBuilder,
+        private _iconsService: IconsService,
+    ) {}
+
+    ngOnInit(): void {
+        this._iconsService.icons
+            .pipe(takeUntil(this._onDestroy))
+            .subscribe((icons) => {
+                const namespace = icons.namespace;
+                const iconsList = icons.list;
+                this.icons = iconsList.map((icon) => ({ namespace, icon }));
+            });
+
+        this.filteredIcons.next(this.icons.slice());
+
+        this.iconFilterCtrl.valueChanges
+            .pipe(takeUntil(this._onDestroy))
+            .subscribe(() => {
+                this.filterIcons();
+            });
+    }
+
+    setIconValue(value: string[]) {
+        this.shortcutForm.controls['icon'].setValue(value.join(' '));
+    }
+
+    protected filterIcons() {
+        if (!this.icons) {
+            return;
+        }
+
+        let search = this.iconFilterCtrl.value;
+        if (!search) {
+            this.filteredIcons.next(this.icons.slice());
+            return;
+        } else {
+            search = search.toLowerCase();
+        }
+
+        this.filteredIcons.next(
+            this.icons.filter(
+                (string) => string.toLowerCase().indexOf(search) > -1,
+            ),
+        );
+    }
+
+    internalSave() {
+        this.save.emit(this.shortcutForm.value);
+        this.shortcutForm.reset();
+    }
 }
--- src/app/layout/common/shortcuts/components/list-shortcuts/list-shortcuts.component.ts
+++ src/app/layout/common/shortcuts/components/list-shortcuts/list-shortcuts.component.ts
@@ -1,5 +1,15 @@
 import { NgFor, NgIf } from '@angular/common';
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, EventEmitter, Input, OnDestroy, OnInit, Output, ViewEncapsulation } from '@angular/core';
+import {
+    ChangeDetectionStrategy,
+    ChangeDetectorRef,
+    Component,
+    EventEmitter,
+    Input,
+    OnDestroy,
+    OnInit,
+    Output,
+    ViewEncapsulation,
+} from '@angular/core';
 import { Shortcut } from '../../shortcuts.types';
 import { ItemShortcutComponent } from '../item-shortcut/item-shortcut.component';
 import { ShortcutsService } from '../../shortcuts.service';
@@ -8,48 +18,49 @@ import { OverlayRef } from '@angular/cdk/overlay';
 import { NoShortcutsComponent } from '../no-shortcuts/no-shortcuts.component';
 
 @Component({
-  selector: 'list-shortcuts',
-  templateUrl: './list-shortcuts.component.html',
-  encapsulation: ViewEncapsulation.None,
-  changeDetection: ChangeDetectionStrategy.OnPush,
-  exportAs: 'list-shortcuts',
-  standalone: true,
-  imports: [NgFor, ItemShortcutComponent, NgIf, NoShortcutsComponent]
+    selector: 'list-shortcuts',
+    templateUrl: './list-shortcuts.component.html',
+    encapsulation: ViewEncapsulation.None,
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    exportAs: 'list-shortcuts',
+    standalone: true,
+    imports: [NgFor, ItemShortcutComponent, NgIf, NoShortcutsComponent],
 })
 export class ListShortcutsComponent implements OnInit, OnDestroy {
+    @Input() mode: Shortcut;
 
-  @Input() mode: Shortcut;
+    public shortcuts: Shortcut[] = [];
 
-  public shortcuts: Shortcut[] = [];
+    @Output() editShortcut: EventEmitter<Shortcut> =
+        new EventEmitter<Shortcut>();
 
-  @Output() editShortcut: EventEmitter<Shortcut> = new EventEmitter<Shortcut>();
+    private _unsubscribeAll: Subject<any> = new Subject<any>();
 
-  private _unsubscribeAll: Subject<any> = new Subject<any>();
+    private _overlayRef: OverlayRef;
 
-  private _overlayRef: OverlayRef;
+    constructor(
+        private _changeDetectorRef: ChangeDetectorRef,
+        private _shortcutsService: ShortcutsService,
+    ) {}
 
-  constructor(private _changeDetectorRef: ChangeDetectorRef,
-    private _shortcutsService: ShortcutsService) { }
-
-  ngOnInit(): void {
-    this._shortcutsService.shortcuts$
-      .pipe(takeUntil(this._unsubscribeAll))
-      .subscribe((shortcuts: Shortcut[]) => {
-        this.shortcuts = shortcuts;
-        console.log(this.shortcuts);
-        this._changeDetectorRef.markForCheck();
-      });
-  }
-
-  ngOnDestroy(): void {
-    this._unsubscribeAll.next(null);
-    this._unsubscribeAll.complete();
-    if (this._overlayRef) {
-      this._overlayRef.dispose();
+    ngOnInit(): void {
+        this._shortcutsService.shortcuts$
+            .pipe(takeUntil(this._unsubscribeAll))
+            .subscribe((shortcuts: Shortcut[]) => {
+                this.shortcuts = shortcuts;
+                this._changeDetectorRef.markForCheck();
+            });
     }
-  }
 
-  trackByFn(index: number, item: any): any {
-    return item.id || index;
-  }
+    ngOnDestroy(): void {
+        this._unsubscribeAll.next(null);
+        this._unsubscribeAll.complete();
+        if (this._overlayRef) {
+            this._overlayRef.dispose();
+        }
+    }
+
+    trackByFn(index: number, item: any): any {
+        return item.id || index;
+    }
 }
